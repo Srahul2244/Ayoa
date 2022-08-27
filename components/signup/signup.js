f9f28411ccf3991df5dbd28ef16c9ba5216fda8f
Karthik <karthikalakunta21@gmail.com>
2022-08-27 13:02:00 +0530

    added localstroge functionality in login and signup

--- components/signup/signup.js
+++ components/signup/signup.js
@@ -1,26 +1,26 @@
-let submit = async() => {
-    let name=document.getElementById("name").value;
+let signup = JSON.parse(localStorage.getItem("signup")) || [];
+let submit = () => {
+  let name=document.getElementById("name").value;
     let email=document.getElementById("email").value;
     let password=document.getElementById("password").value;
-    if(name==="" || email=="" || password==""){
-        alert("fILL the form")
-        return
-    }const d = new Date();
-    let day = d.getTime()
-    console.log(name,email,password)
-    let send_data={
-        id:day,
-        name,
-        email,
-        password
-    }
-    let res = await fetch("http://localhost:3000/signup/", {
-    method: "POST",
-    body: JSON.stringify(send_data),
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
-  let data = await res.json();
-  window.location.href="./login.html"
+  if (
+    name == "" ||
+    email == "" ||
+    password == ""
+  ) {
+    alert("Fill the form");
+  } else {
+    let Email =email;
+    let Name = name;
+    let Password = password;
+    let obj = {
+      email: Email,
+      name: Name,
+      password: Password,
+    };
+    signup.push(obj);
+    localStorage.setItem("signup", JSON.stringify(signup));
+    window.location.href = "signin.html";
+  }
 }
+console.log(signup);
