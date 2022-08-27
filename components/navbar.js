cd0b09d75e5f979d6dff1d07c9f51bf9841460e4
Karthik <karthikalakunta21@gmail.com>
2022-08-27 09:53:40 +0530

    issue fixed

--- components/navbar.js
+++ components/navbar.js
@@ -1,40 +1,42 @@
 function navbar(){
     return `<nav>
-    <div>
-      <label class="logo">Ayoa</label>
-    </div>
-    <div>
-      <ul>
-          <li><a class="active" href="#">Home</a></li>
+    <div class="navbar">
+      <i class='bx bx-menu'></i>
+      <div class="logo"><a href="#">Ayoa</a></div>
+      <div class="nav-links">
+        <div class="sidebar-logo">
+          <span class="logo-name">Ayoa</span>
+          <i class='bx bx-x' ></i>
+        </div>
+        <ul class="links">
+          <li><a href="#">HOME</a></li>
           <li>
-            <a href="#"
-              >Products
-              <i class="fas fa-caret-down"></i>
-            </a>
-            <ul>
-              <li><a href="./MindMap.html">--Ayoa Mind Map</a></li>
-              <li><a href="#">--Ayoa Task</a></li>
-              <li><a href="#">--Ayoa Ultimate</a></li>
+            <a href="#">Products</a>
+            <i class='bx bxs-chevron-down htmlcss-arrow arrow  '></i>
+            <ul class="htmlCss-sub-menu sub-menu">
+              <li><a href="#">Ayoa Mind Map</a></li>
+              <li><a href="#">Ayoa Task</a></li>
+              <li><a href="#">Ayoa Ultimate</a></li>
             </ul>
           </li>
           <li><a href="#">Inclusivity</a></li>
           <li>
-            <a href="#"
-              >Resources
-              <i class="fas fa-caret-down"></i>
-            </a>
-            <ul>
-              <li><a href="./Blog.html">--Ayoa Blog</a></li>
-              <li><a href="#">--Press & Articles</a></li>
-              <li><a href="#">--Keynote Speakers</a></li>
-              <li><a href="#">--Help Center</a></li>
+            <a href="#">Resources</a>
+            <i class='bx bxs-chevron-down htmlcss-arrow arrow  '></i>
+            <ul class="htmlCss-sub-menu sub-menu">
+              <li><a href="#">Ayoa Blog</a></li>
+              <li><a href="#">Press & Articles</a></li>
+              <li><a href="#">Keynote Speaker</a></li>
+              <li><a href="#">Help Center</a></li>
             </ul>
           </li>
           <li><a href="#">Pricing</a></li>
-          <li><a href="./login.html">Log in</a></li>
         </ul>
+      </div>
+      <div class="login">
+        <a href="">Login</a>
+      </div>
     </div>
-
   </nav>`
 }
 export default navbar; 
